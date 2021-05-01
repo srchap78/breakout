83d4d4986ef0095b4a75f8cc93b108d947067ce3
srchap78 <srchap78@gmail.com>
2021-05-01 21:27:26 +0200

    added ball and moves around inside of grid

--- index.js
+++ index.js
@@ -1,109 +1,78 @@
 let grid = document.querySelector(".grid");
 
-
 //values from css of .block
-let blockWidth = 100
-let blockHeight = 25
-
-//User positions
-let userStartPosition = [510,20]
-let userCurrentPosition = userStartPosition
+let boardWidth = 1120;
+let boardHeight = 500;
+let ballDiameter = 15;
 
 //Ball positions
-let ballStartPosition = [553, 150]
-let ballCurrentPosition = ballStartPosition
-
-//Class to build and position blocks in the grid. Uses the bottom left as an anchor.
-class Block {
-    constructor(xAxis, yAxis){
-this.bottomLeft = [xAxis, yAxis]
-this.bottomRight = [xAxis + blockWidth, yAxis]
-this.topLeft = [xAxis, yAxis + blockHeight]
-this.topRight = [xAxis + blockWidth, yAxis + blockHeight]
-    }
-}
-
-//Array to hold a 10X5 grid of blocks
-let blocks = [
-     new Block(15,460),
-     new Block(125,460),
-     new Block(235,460),
-     new Block(345,460),
-     new Block(455,460),
-     new Block(565,460),
-     new Block(675,460),
-     new Block(785,460),
-     new Block(895,460),
-     new Block(1005,460),
-     
-     new Block(15,430),
-     new Block(125,430),
-     new Block(235,430),
-     new Block(345,430),
-     new Block(455,430),
-     new Block(565,430),
-     new Block(675,430),
-     new Block(785,430),
-     new Block(895,430),
-     new Block(1005,430),
-
-     new Block(15,400),
-     new Block(125,400),
-     new Block(235,400),
-     new Block(345,400),
-     new Block(455,400),
-     new Block(565,400),
-     new Block(675,400),
-     new Block(785,400),
-     new Block(895,400),
-     new Block(1005,400),
-
-     new Block(15,370),
-     new Block(125,370),
-     new Block(235,370),
-     new Block(345,370),
-     new Block(455,370),
-     new Block(565,370),
-     new Block(675,370),
-     new Block(785,370),
-     new Block(895,370),
-     new Block(1005,370),
-
-     new Block(15,340),
-     new Block(125,340),
-     new Block(235,340),
-     new Block(345,340),
-     new Block(455,340),
-     new Block(565,340),
-     new Block(675,340),
-     new Block(785,340),
-     new Block(895,340),
-     new Block(1005,340),
-]
-
-//creates blocks using Block class
-function addBlock() {
-  for (let i = 0; i < blocks.length; i++) {
-    let block = document.createElement("div");
-    block.classList.add("block");
-    block.style.left = blocks[i].bottomLeft[0] + 'px'
-    block.style.bottom = blocks[i].bottomLeft[1] + 'px'
-    grid.appendChild(block);
-      
-  }
-}
-addBlock()
-
-//User block
-let user = document.createElement('div')
-user.classList.add('user')
-user.style.left = userCurrentPosition[0] + 'px'
-user.style.bottom = userCurrentPosition[1] + 'px'
-grid.appendChild(user)
+let ballStartPosition = [553, 150];
+let ballCurrentPosition = ballStartPosition;
+let xDirection = 3;
+let yDirection = 3;
 
 //Ball
-let ball = document.createElement('div')
-ball.classList.add('ball')
-ball.style.left= ballCurrentPosition[0] + 'px'
-ball.style.bottom= ballCurrentPosition[1] + 'px'
-grid.appendChild(ball)
+let ball = document.createElement("div");
+ball.classList.add("ball");
+grid.appendChild(ball);
+drawBall();
+
+//Animate the ball
+function drawBall() {
+  ball.style.left = ballCurrentPosition[0] + "px";
+  ball.style.bottom = ballCurrentPosition[1] + "px";
+}
+
+function moveBall() {
+  ballCurrentPosition[0] += xDirection;
+  ballCurrentPosition[1] += yDirection;
+  drawBall();
+  checkForCollisions();
+}
+
+//The setInterval() method calls a function or evaluates an expression at specified intervals (in milliseconds).The setInterval() method will continue calling the function until clearInterval() is called, or the window is closed.
+let timer = setInterval(moveBall, 25);
+
+//Collision checks
+
+function checkForCollisions() {
+  
+  //check for hitting walls
+  if (
+    ballCurrentPosition[0] >= boardWidth - ballDiameter ||
+    ballCurrentPosition[1] >= boardHeight - ballDiameter ||
+    ballCurrentPosition[0] <= 0 ||
+    //bottom wall check in place just to keep the ball moving - change this to stop the ball moving to show loss
+    ballCurrentPosition[1] <= 0
+  ) {
+    changeDirection();
+  }
+  //check for hitting user
+  if (ballCurrentPosition[1] == (userCurrentPosition - ballDiameter)) {
+    changeDirection();
+  }
+}
+
+//change the ball's direction depending on which way it is currently moving
+function changeDirection() {
+  //if moving to the right and up
+  if (xDirection === 3 && yDirection === 3) {
+    yDirection = -3;
+    return;
+  }
+  //if moving to the right and down
+  if (xDirection === 3 && yDirection === -3) {
+    xDirection = -3;
+    return;
+  }
+  //if moving to the left and down
+  if (xDirection === -3 && yDirection === -3) {
+    yDirection = 3;
+    return;
+  }
+  //if moving to the left and up
+  if (xDirection === -3 && yDirection === 3) {
+    xDirection = 3;
+    return;
+  }
+}
---
+++ readme.md
@@ -0,0 +1,7 @@
+# Breakout version 1.0
+### Wrote in basic HTML, CSS and Javascript
+#### May 1, 2021  
+
+Fun. Learned event listeners and animating elements. Logic was simple but fun to figure out e.g. starting places for the blocks.  
+  
+Watched a tutorial on youtube but then made it my own after understanding the concepts.  
